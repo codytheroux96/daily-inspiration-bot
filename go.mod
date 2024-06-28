56eef032aae0eadd5aae5c54a246ab5e215c1b43
codytheroux96 <cody.theroux3@gmail.com>
2024-06-27 22:04:43 -0400

    added environment variables, made quote struct

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/codytheroux96/daily-inspiration-bot
 
 go 1.22.0
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Quote struct {
+	ID     int
+	Text   string `json:"text"`
+	Author string `json:"author"`
+	Posted bool   `json:"posted"`
+}
+
+func main() {
+	var db *sql.DB
+	var err error
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		log.Fatal("TOKEN is not found in the environment")
+	}
+
+	channelID := os.Getenv("CHANNELID")
+	if channelID == "" {
+		log.Fatal("CHANNELID is not found in the environment")
+	}
+}
